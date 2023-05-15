1fa45d1ee9b68080e9348530cf1f93fe4eca60d2
theZalmanian <thezalmanian@gmail.com>
2023-05-15 01:50:13 -0700

    Added Method to setup default role

--- LectureManager/Models/IdentityHelper.cs
+++ LectureManager/Models/IdentityHelper.cs
@@ -4,10 +4,22 @@ namespace LectureManager.Models
 {
     public static class IdentityHelper
     {
+        /// <summary>
+        /// Represents the Instructor role
+        /// </summary>
         public const string Instructor = "Instrctor";
+
+        /// <summary>
+        /// Represents the Student role
+        /// </summary>
         public const string Student = "Student";
 
-        public static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        /// <summary>
+        /// When called: Sets up all the site roles
+        /// </summary>
+        /// <param name="provider">A Service Provider</param>
+        /// <param name="roles">An array containing all site roles</param>
+        public static async Task SetupRoles(IServiceProvider provider, params string[] roles)
         {
             // Setup role manager
             RoleManager<IdentityRole> roleManager = provider.GetService<RoleManager<IdentityRole>>();
@@ -24,6 +36,36 @@ namespace LectureManager.Models
                     // Create it
                     await roleManager.CreateAsync(new IdentityRole(currRole));
                 }
+            }
+        }
+
+        /// <summary>
+        /// Creates a default user for the given role as long as one does not already exist
+        /// </summary>
+        /// <param name="provider">A Service Provider</param>
+        /// <param name="role">The role the default user would belong to</param>
+        public static async Task CreateDefaultUser(IServiceProvider provider, string role)
+        {
+            // Setup user manager
+            UserManager<IdentityUser> userManager = provider.GetService<UserManager<IdentityUser>>();
+
+            // Get the number of users belonging to the current role
+            int numUsersInRole = (await userManager.GetUsersInRoleAsync(role)).Count;
+
+            // If no users exist in that role
+            if(numUsersInRole == 0)
+            {
+                // Setup the default user for that role
+                var defaultUser = new IdentityUser()
+                {
+                    Email = "instructor@lecturemanager.com",
+                    UserName = "Admin"
+                };
+
+                await userManager.CreateAsync(defaultUser, "Pas.sWork5ngHr$der");
+
+                // Make them the given role
+                await userManager.AddToRoleAsync(defaultUser, role);
             }
         }
     }
