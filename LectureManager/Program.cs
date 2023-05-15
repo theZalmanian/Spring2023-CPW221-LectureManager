a76a1a4fb6362e5340c163bac88db0238a93eeec
theZalmanian <thezalmanian@gmail.com>
2023-05-15 00:39:52 -0700

    Added default login options for later customiziation access

--- LectureManager/Program.cs
+++ LectureManager/Program.cs
@@ -20,6 +20,27 @@ namespace LectureManager
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Default Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 12; // modified min pass length from 6 to 12
+                options.Password.RequiredUniqueChars = 1;
+
+                // Default Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Default User settings.
+                options.User.AllowedUserNameCharacters =
+                        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
